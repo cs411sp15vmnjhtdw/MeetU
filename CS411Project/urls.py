d29aa76d53358fda123e47217db16080fbbdf363
Neville Jos <josneville@yahoo.com>
2015-10-16 14:19:59 -0500

    HTML url added

--- CS411Project/urls.py
+++ CS411Project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from meetu import views
+
 urlpatterns = [
+    url(r'^/*', include('meetu.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/*', include('meetu.urls')),
 ]
