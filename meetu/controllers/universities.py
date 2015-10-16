1d5b85fd7129ac76d9aadaff631dc8689c204232
Neville Jos <josneville@yahoo.com>
2015-10-16 15:28:56 -0500

    post to POST

--- meetu/controllers/universities.py
+++ meetu/controllers/universities.py
@@ -2,9 +2,11 @@ import json
 
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
 from ..models import University
 
+@csrf_exempt
 def index(request):
     if request.method == 'GET':
         return all(request)
@@ -18,8 +20,8 @@ def all(request):
     return JsonResponse(universities, safe=False)
 
 def create(request):
-    if request.POST["city"] and request.POST["state"] and request.post["name"]:
-        u = University.objects.create(name=request.post["name"], city=request.post["city"], state=request.post["state"])
+    if request.POST["city"] and request.POST["state"] and request.POST["name"]:
+        u = University.objects.create(name=request.POST["name"], city=request.POST["city"], state=request.POST["state"])
         u.save()
         return HttpResponse("Success")
     else:
