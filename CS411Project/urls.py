b991dfea3ad39e9e7ac55be6c92cb544563ec813
DandiW <dwang15@icloud.com>
2015-10-16 15:10:54 -0500

    static files setup

--- CS411Project/urls.py
+++ CS411Project/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
-from meetu import views
+from meetu import urls as meetu_urls
 
 urlpatterns = [
-    url(r'^/*', include('meetu.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^/*', include(meetu_urls)),
 ]
--- meetu/models.py
+++ meetu/models.py
@@ -4,3 +4,10 @@ class University(models.Model):
     name = models.CharField(max_length=200)
     state = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
+
+class Student(models.Model):
+	email = models.CharField(max_length=200)
+	name = models.CharField(max_length=200)
+	age = models.IntegerField()
+	gender = models.BooleanField(default=False)
+	lastAccessTime = models.DateField()
