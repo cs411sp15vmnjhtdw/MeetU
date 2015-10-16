2c5c29a892f51b6e6956be4eafeb0ce7a9516907
Neville Jos <josneville@yahoo.com>
2015-10-16 15:37:38 -0500

    one migration

--- meetu/models.py
+++ meetu/models.py
@@ -6,8 +6,9 @@ class University(models.Model):
     city = models.CharField(max_length=200)
 
 class Student(models.Model):
-	email = models.CharField(max_length=200)
-	name = models.CharField(max_length=200)
-	age = models.IntegerField()
-	gender = models.BooleanField(default=False)
-	lastAccessTime = models.DateField()
+    university = models.ForeignKey(University)
+    email = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    gender = models.BooleanField(default=False)
+    lastAccessTime = models.DateField()
